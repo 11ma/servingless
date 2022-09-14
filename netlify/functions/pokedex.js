c0a1a78e10c96b2e351d62be0e8109a77e99d558
Michael A <mikeade08l@GMAIL.COM>
2022-09-14 07:46:43 +0100

    getting custom data with serverless function

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,18 @@
 import fetch from "node-fetch"
 
-export async function handler() {
-	const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto"
+export async function handler(event, context) {
+	const eventBody = JSON.parse(event.body)
+	const POKE_API = "https://pokeapi.co/api/v2/pokedex/" + eventBody.region
+
+	console.log(POKE_API)
 
 	const response = await fetch(POKE_API)
 	const data = await response.json()
 
 	return {
 		statusCode: 200,
-		body: JSON.stringify(data),
+		body: JSON.stringify({
+			pokemon: data.pokemon_entries,
+		}),
 	}
 }
